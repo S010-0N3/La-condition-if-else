ded4067be71b03cd99575e49fae0aced4fd566fb
Solo one one <fally59100@gmail.com>
2021-03-09 10:55:13 +0000

    ajout explication

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ planete_names = ["Mercure","Terre","Venus","Mars","Jupiter","Saturne","Neptune",
 
 long_names =[]
 short_names = []
-
+### len sert a compter les elements ,la (u) etant chaque planete, il compte chaque lettre
 for u in planete_names:
   if len(u) > 5:
     long_names.append(u)
@@ -19,12 +19,19 @@ print(short_names)
 
 fruits = ["Citron","Banane","Pomme","Pomme","Pomme","Banane","Banane","Pomme"]
 
+#liste vide
 fruits_count = {}
 
+#boucle pour verifier et ajoute +1 a fruit count, si on regarde c'est l'ajout d'un element de dictionnaire:
 for item in fruits:
   if item in fruits_count:
     fruits_count[item] = fruits_count[item] + 1
   else:
     fruits_count[item] = 1
-
 print(fruits_count)
+
+#RAPPEL : ajout element dand une liste
+op = {}
+op["tarte"]= 5
+print(op)
+
